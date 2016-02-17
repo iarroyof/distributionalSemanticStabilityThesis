f038fb6577d963b5bc8ab397b9d0012c4d9a1f72
iarroyof <axayakac@gmail.com>
2016-02-16 19:22:34 -0600

    Able to catch the broken pipe error, however nt fixed yet.

--- mklCall.py
+++ mklCall.py
@@ -92,8 +92,8 @@ if __name__ == '__main__':
         path = list(literal_eval(args.current_path))
         
         [performance, model, output] = mkl_learning_pool(path)
-        sys.stdout.flush()
         stdout.write('%s;%s;%s;%s\n' % (performance, path, model, output))
+        stdout.flush()
 
         # This script (mklCall.py) is called multiple times in learning mode, so multiple outputs are written to the
         # stdout. These outputs are managed by the reducer script (mklReducer.py).
